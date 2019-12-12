eb0547c8a7c6a059516852a84f6a5c296c8e172a
bhyang <wa@q.com>
2019-12-12 17:53:08 +0800

    dd

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/service/CourseService.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/service/CourseService.java
@@ -1,9 +1,12 @@
 package com.xuecheng.manage_course.service;
 
+import com.xuecheng.framework.domain.course.CourseBase;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
 import com.xuecheng.manage_course.dao.TeachplanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CourseService {
@@ -14,4 +17,9 @@ public class CourseService {
         TeachplanNode teachplanNode = teachplanMapper.selectList(courseId);
         return teachplanNode;
     }
+
+//    public String getTeachplanRoot(String courseId) {
+//        // 校验课程id
+//        Optional<CourseBase> optional = courseBaseRepository.findById(courseId)
+//    }
 }
