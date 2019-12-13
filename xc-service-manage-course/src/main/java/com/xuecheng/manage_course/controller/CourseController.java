10f908bd503eaaa9b512dd831c965818a397d7f5
bhyang <wa@q.com>
2019-12-13 18:12:11 +0800

    认证

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/controller/CourseController.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/controller/CourseController.java
@@ -1,7 +1,9 @@
 package com.xuecheng.manage_course.controller;
 
 import com.xuecheng.api.course.CourseControllerApi;
+import com.xuecheng.framework.domain.course.Teachplan;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.manage_course.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +19,14 @@ public class CourseController implements CourseControllerApi {
 
 
     @GetMapping("/teachplan/list/{courseId}")
+    @Override
     public TeachplanNode findTeachplanList(@PathVariable("courseId") String courseId) {
         return courseService.findTeachplanList(courseId);
 
+    }
+
+    @Override
+    public ResponseResult addTeachplan(Teachplan teachPlan) {
+        return null;
     }
 }
